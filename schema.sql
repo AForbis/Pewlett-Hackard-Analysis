941c0a495f0b0b25dc7f3ccf607eccb7289fddd6
AlexF <alexforbis@utexas.edu>
2021-02-05 17:30:41 -0600

    added near retirees to new table

--- schema.sql
+++ schema.sql
@@ -54,6 +54,40 @@ CREATE TABLE titles (
     PRIMARY KEY (emp_no, from_date)
 );
 
-select * from dept_employees;
+-- employees born between 1952-1955
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
 
-commit;
+-- employees born only in 1952
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
+
+
+
+
+
+
