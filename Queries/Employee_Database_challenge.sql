8a505ab11d80cf6d74ec9cb55a71341497ba6043
AlexF <alexforbis@utexas.edu>
2021-02-06 12:35:37 -0600

    finishing d2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -11,17 +11,17 @@ INTO retirement_titles
 FROM employees as e
 INNER JOIN titles as t
 ON (e.emp_no = t.emp_no)
-WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
 
--- Get Unique from Retirement Titles - Use Dictinct with Orderby to remove duplicate rows
+-- Get Unique from Retirement Titles - Use Distinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (emp_no) emp_no,
 	first_name,
 	last_name,
 	title
 INTO unique_titles
 FROM retirement_titles
-ORDER BY emp_no, emp_no ASC, to_date DESC;
+ORDER BY emp_no ASC, to_date DESC;
 
 -- Get Summary of Unique Retiring Titles
 SELECT COUNT (title), title
@@ -30,10 +30,12 @@ FROM unique_titles
 GROUP BY title
 ORDER BY count DESC;
 
+
+
 -- Deliverable #2
 -- Create a mentorship eligibility table
 
-SELECT DISTINCT ON (emp_no) e.emp_no,
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
 	e.first_name,
 	e.last_name,
 	e.birth_date,
@@ -48,7 +50,7 @@ INNER JOIN titles as t
 ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31') 
 	AND (de.to_date = '9999-01-01')
-ORDER BY emp_no;
+ORDER BY e.emp_no;
 
 commit;
 
