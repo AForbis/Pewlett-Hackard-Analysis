df03ddb9f60a33b113dab16bddb1e3f4a9d99296
AlexF <alexforbis@utexas.edu>
2021-02-06 12:02:10 -0600

    final step of Del #2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -30,10 +30,25 @@ FROM unique_titles
 GROUP BY title
 ORDER BY count DESC;
 
+-- Deliverable #2
+-- Create a mentorship eligibility table
+
+SELECT DISTINCT ON (emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+    de.from_date,
+    de.to_date,
+	t.title
+--INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_employees as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31') 
+	AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
 
 commit;
-
-
-
-
 
