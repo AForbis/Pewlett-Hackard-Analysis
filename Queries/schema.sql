f61b28d9ec8a61bb43357f52290ebbe56668a6c4
AlexF <alexforbis@utexas.edu>
2021-02-06 09:17:28 -0600

    starting skill drill 1 in 7.3.6

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -144,6 +144,7 @@ FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
+-- List of general employee info
 SELECT e.emp_no,
 	e.first_name,
 	e.last_name,
@@ -161,9 +162,44 @@ WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 	AND (de.to_date = '9999-01-01');
 
 
-select * from emp_info;
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+
+-- List of department retirees
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_employees AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+
+select * from manager_info;
 
 commit;
+
+
+
+
+
+
+
 
 
 
