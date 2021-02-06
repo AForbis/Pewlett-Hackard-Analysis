f54fbe5d98e1afc11aa97e897798588bb4ea9ce0
AlexF <alexforbis@utexas.edu>
2021-02-06 11:22:24 -0600

    finished deliverable #1

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,4 +1,6 @@
---Deliverable #1
+-- Deliverable #1
+
+-- Get Retirement Titles
 SELECT e.emp_no,
 	e.first_name,
 	e.last_name,
@@ -12,7 +14,7 @@ ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
 
--- Use Dictinct with Orderby to remove duplicate rows
+-- Get Unique from Retirement Titles - Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (emp_no) emp_no,
 	first_name,
 	last_name,
@@ -20,6 +22,13 @@ SELECT DISTINCT ON (emp_no) emp_no,
 INTO unique_titles
 FROM retirement_titles
 ORDER BY emp_no, emp_no ASC, to_date DESC;
+
+-- Get Summary of Unique Retiring Titles
+SELECT COUNT (title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
 
 
 commit;
