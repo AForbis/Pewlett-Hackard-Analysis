24c3bda40ae69ca0c2fcf20a2bd4d438a78351c4
AlexF <alexforbis@utexas.edu>
2021-02-06 11:12:06 -0600

    On Del 1: #15

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,9 +1,30 @@
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+--Deliverable #1
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+    t.from_date,
+    t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
 
-INTO nameyourtable
-FROM _______
-ORDER BY _____, _____ DESC;
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+	first_name,
+	last_name,
+	title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, emp_no ASC, to_date DESC;
+
+
+commit;
+
+
+
+
+
